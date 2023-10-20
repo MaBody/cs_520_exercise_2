8c9c936966b448990f67727a22d49ef3902e3082
Mattis Bodynek <mattisbodynek@gmail.com>
2023-10-20 12:17:06 -0400

    Added first test cases for statement and decision coverage

--- test_suit/test_decisionCoverage.py
+++ test_suit/test_decisionCoverage.py
@@ -5,6 +5,13 @@ from isTriangle import Type
 
 class test_decisionCoverage(unittest.TestCase):
     def decision_tests(self):
+        # triangles = dict(eq_1=((2, 2, 2), Triangle.Type.EQUILATERAL),
+        #                  sc_1=((2, 3, 4), Triangle.Type.SCALENE),
+        #                  is_1=((3, 3, 5), Triangle.Type.ISOSCELES),
+        #                  is_2=((3, 5, 3), Triangle.Type.ISOSCELES),
+        #                  is_3=((5, 3, 3), Triangle.Type.ISOSCELES),
+        #                  in_1=((1, 2, 5), Triangle.Type.INVALID),
+        #                  )
         triangles = dict(eq_1=((2, 2, 2), Triangle.Type.EQUILATERAL),
                          sc_1=((2, 3, 4), Triangle.Type.SCALENE),
                          is_1=((3, 3, 5), Triangle.Type.ISOSCELES),
